69f61d6322298c4841a46d6b1f33e832f546058a
charles-l <chuckiels2011@gmail.com>
2015-01-13 15:54:35 -0500

    end of part 5

--- main.lua
+++ main.lua
@@ -44,19 +44,20 @@ function get_input()
     return i
 end
 
--- Create a room. exit_command is a string. exit_command will be the command
--- that cuases us to leave the room and break the loop.
--- t is a key/value list of commands possible and the printed response.
--- {'command' = 'response', 'command2' = 'response2'}
-function room(exit_command, t)
+function room(t)
+    room_exit = false
     input = ""
-    while input ~= exit_command do
+    while not room_exit do
         input = get_input()
         valid = false
 
         for k,v in pairs(t) do
-            if input == k then
-                print(v)
+            if input == string.gsub(k, "_", " ") then
+                if type(v) == "string" then
+                    print(v)
+                else
+                    v()
+                end
                 valid = true
             end
         end
@@ -69,79 +70,61 @@ function room(exit_command, t)
     end
 end
 
-room_com = {inspect = "You're in a cave. There is an exit.", exit = "You leave the cave."}
-room("exit", room_com)
+room_com = {inspect = "You're in a cave. There is an exit.",
+exit = function()
+    print("You leave the cave.")
+    room_exit = true
+end}
+room(room_com)
 
-------------------------------------------------
--- THE FOLLOWING WILL BE UPDATED NEXT EPISODE --
-------------------------------------------------
-
-input = ""
-while input ~= "follow path" do
-    input = get_input()
-
-    if input == "inspect" then
-        print("There is a cave behind you. You're on a cliff. There is a pathway leading to the south.")
-    elseif input == "follow path" then
+room_com2 = {
+    inspect = "There is a cave behind you. You're on a cliff. There is a pathway leading to the south.",
+    follow_path = function()
         print("You follow the path")
         print("A troll appears wielding an ax")
         print("What do you want to do? (run away, attack)")
         tinput = io.read()
         if tinput == "attack" then
             print("You smack with you're sword and it falls dead")
+            room_exit = true
         elseif tinput == "run away" then
             print("You cowardly turn your back to flee but it smacks and steals your coin")
             pop_inv("coin")
+            room_exit = true
         else
             print("You stand there. It stabs you. You die. Game over")
             os.exit()
         end
-    elseif input == "inv" then
-        get_inv()
-    else
-        err_command()
     end
-end
+}
 
-input = ""
+room(room_com2)
 
--- A boolean value can only have two values: true or false
--- This works great for the 'have_key' variable, because
--- there are only two possibilities. You have the key or
--- you don't.
 have_key = false
-
--- This statement means: while this statement: "input is equal to 'open gate' and have_key is true" is not met, loop.
-while not (input == "open gate" and have_key == true) do
-    input = get_input()
-
-    if input == "inspect" then
-        -- If we don't have the key, let us know that it's on the ground.
+room_com3 = {
+    inspect = function()
         if have_key == false then
             print("There is a path behind you. There is a locked gate in front of you. There is a key on the ground.")
         else
             -- If we do have the key, don't say there's a key on the ground.
             print("There is a path behind you. There is a locked gate in front of you.")
         end
-    elseif input == "grab key" then
-        -- We now have the key. Set the variable to true.
+    end,
+    grab_key = function()
         have_key = true
         print("You got the key")
-    elseif input == "open gate" then
+    end,
+    open_gate = function()
         if have_key == false then
             print("It's locked")
         elseif have_key == true then
             print("You open the gate and leave.")
+            room_exit = true
         end
-    elseif input == "pick up magic" then
-        print("You obtain magic")
-        push_inv("MAGIC")
-    elseif input == "inv" then
-        get_inv()
-    else
-        err_command()
     end
-end
+}
+
+room(room_com3)
 
 -- VICTORY!
 print("You escaped.")
---
+++ slideshows/part_5/part_5.md
@@ -0,0 +1,21 @@
+##Intro to
+##Programming
+##in Lua
+Part 5
+
+
+- Functions *in* tables
+- Miscellaneous cleanup
+
+
+# Functions
+# in tables
+```lua
+x = {}
+x.a = function()
+    print("a")
+end
+```
+
+
+## Let's go!
